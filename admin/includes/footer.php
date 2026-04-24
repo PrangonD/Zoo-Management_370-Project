9d30b58091264a737021fa0d220d3bc10e163016
PrangonD <prangon436@gmail.com>
2026-04-24 17:48:13 +0600

    add my

---
+++ admin/includes/footer.php
@@ -0,0 +1,9 @@
+ <footer class="sticky-footer bg-white">
+ 	<div class="container my-auto">
+ 		<div class="copyright text-center my-auto">
+ 			<span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
+ 				<b><a href="https://Zoo_management_system" target="_blank">Zoo Inc</a></b>
+ 			</span>
+ 		</div>
+ 	</div>
+ </footer>
